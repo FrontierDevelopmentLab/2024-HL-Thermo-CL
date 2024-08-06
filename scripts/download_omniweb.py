470b50056048de3174e59eb1a99d8f215f92e496
Will Fawcett <willfaw@gmail.com>
2024-08-06 15:59:33 +0000

    WIP: download SOHO and omniweb

--- scripts/download_omniweb.py
+++ scripts/download_omniweb.py
@@ -2,19 +2,21 @@ import os
 import subprocess
 from datetime import datetime
 
-# Define the initial start date and the end date
-years=range(2000,2025)
-year_end=2024
-start_date = datetime.strptime("20000729", "%Y%m%d")
-end_date = datetime.strptime("20240531", "%Y%m%d")
+def get_all_years() -> list[int]:
+    """
+    Get all years from 2000 until the present. 
+    """
+    now = datetime.now()
+    this_year = now.year
+    print(now, this_year)
 
-base_url = "https://omniweb.gsfc.nasa.gov/cgi/nx1.cgi"
-fixed_params = "activity=retrieve&res=min&spacecraft=omni_min&vars=11&vars=12&vars=13&vars=14&vars=15&vars=16&vars=17&vars=18&vars=19&vars=20&vars=21&vars=22&vars=23&vars=24&vars=25&vars=26&vars=27&vars=28&vars=29&vars=30&vars=31&vars=32&vars=33&vars=34&vars=35&vars=36&vars=37&vars=38&vars=39&vars=40&vars=41&vars=42&scale=Linear&ymin=&ymax=&view=0&charsize=&xstyle=0&ystyle=0&symbol=0&symsize=&linestyle=solid&table=0&imagex=640&imagey=480&color=&back="
-omiweb_data_dir='../data/omniweb_data_raw'
-os.makedirs(omiweb_data_dir, exist_ok=True)  # Ensure local directory exists
+    all_years = list(range(2000, this_year+1))
+    print(all_years)
+    return all_years
 
-# Loop over each year and generate the command
-for year in years:
+
+def get_year_start_and_end_dates(year) -> tuple[str]:
+
     if year==2000:
         start_date = str(year)+"0101"
         end_date = str(year)+"0729"
@@ -24,17 +26,42 @@ for year in years:
     else:
         start_date = str(year)+"0101"
         end_date = str(year)+"1231"
+
+    return start_date, end_date
+
+def main():
+
+    # 
+    base_url = "https://omniweb.gsfc.nasa.gov/cgi/nx1.cgi"
+    fixed_params = "activity=retrieve&res=min&spacecraft=omni_min&vars=11&vars=12&vars=13&vars=14&vars=15&vars=16&vars=17&vars=18&vars=19&vars=20&vars=21&vars=22&vars=23&vars=24&vars=25&vars=26&vars=27&vars=28&vars=29&vars=30&vars=31&vars=32&vars=33&vars=34&vars=35&vars=36&vars=37&vars=38&vars=39&vars=40&vars=41&vars=42&scale=Linear&ymin=&ymax=&view=0&charsize=&xstyle=0&ystyle=0&symbol=0&symsize=&linestyle=solid&table=0&imagex=640&imagey=480&color=&back="
+    omiweb_data_dir='/shared/raw/omniweb'
+
+    # Define the initial start date and the end date
+    years = get_all_years()
+    start_date = datetime.strptime("20000729", "%Y%m%d")
+    end_date = datetime.strptime("20240531", "%Y%m%d")
+
+
+    os.makedirs(omiweb_data_dir, exist_ok=True)  # Ensure local directory exists
+
+    # Loop over each year and generate the command
+    for year in years:
+        start_date, end_date = get_year_start_and_end_dates(year)
     
-    
-    # Generate the wget command
-    output_file = os.path.join(omiweb_data_dir,f"data_{start_date}_{end_date}.txt")
-    print('check if already exists')
-    if os.path.exists(output_file)==False:
-        wget_command = f"sudo wget --post-data \"{fixed_params}&start_date={start_date}&end_date={end_date}\" {base_url} -O {output_file}"
-    
-        # Execute the wget command
-        print(f"Running command: {wget_command}")
-        subprocess.run(wget_command, shell=True)
-    else:
-        print('file already exists, moving to the next one')
+        # Generate the wget command
+        output_file = os.path.join(omiweb_data_dir,f"data_{start_date}_{end_date}.txt")
+        print('check if already exists')
+  
+        if os.path.exists(output_file)==False:
+            wget_command = f"sudo wget --post-data \"{fixed_params}&start_date={start_date}&end_date={end_date}\" {base_url} -O {output_file}"
         
+            # Execute the wget command
+            print(f"Running command: {wget_command}")
+            subprocess.run(wget_command, shell=True)
+
+        else:
+            print('file already exists, moving to the next one')
+        
+
+if __name__ == "__main__":
+    main()
--- scripts/download_soho.py
+++ scripts/download_soho.py
@@ -3,36 +3,69 @@ import os
 import time
 from concurrent.futures import ThreadPoolExecutor, as_completed
 
-output_dir = "/home/ga00693/2024-HL-Thermo-CL/data/soho_data_raw"
-base_url = "https://lasp.colorado.edu/eve/data_access/eve_data/lasp_soho_sem_data/long/15_sec_avg/"
+import datetime
 
-years = [2000, 2001, 2002, 2003, 2004, 2005, 2006, 2007, 2008, 2009, 2010, 2011, 
-        2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022, 2023, 2024]
 
-def download_year_data(year):
-    directory = f"{output_dir}/{year}"
-    os.makedirs(directory, exist_ok=True)
-    wget_command = [
-        "wget",
-        "-r",
-        "-np",
-        "-nH",
-        "--cut-dirs=7",
-        "-A",
-        "*.00",
-        "--reject",
-        "index.html*",
-        "--tries=5",  # Retry up to 5 times
-        "--waitretry=2",  # Wait 2 seconds between retries
-        f"{base_url}{year}/",
-    ]
+class SohoDownloader:
+    def __init__(self, output_dir: str, base_url: str):
+        self.output_dir = output_dir
+        self.base_url = base_url
 
-    subprocess.run(wget_command, cwd=directory)
-    return year
+    def download_one_year_data(self, year: int):
 
-# let's parallelize the download to save time
-with ThreadPoolExecutor() as executor:  # in case needed, use the max_workers argument to limit the number of workers
-    futures = [executor.submit(download_year_data, year) for year in years]
-    for future in as_completed(futures):
-        year = future.result()
-        print(f"Completed download for year: {year}")
+        directory = f"{self.output_dir}/{year}"
+        os.makedirs(directory, exist_ok=True)
+        wget_command = [
+            "wget",
+            "-r",
+            "-np",
+            "-nH",
+            "--cut-dirs=7",
+            "-A",
+            "*.00",
+            "--reject",
+            "index.html*",
+            "--tries=5",  # Retry up to 5 times
+            "--waitretry=2",  # Wait 2 seconds between retries
+            f"{self.base_url}{year}/",
+        ]
+
+        subprocess.run(wget_command, cwd=directory, stdout=subprocess.DEVNULL)
+        return year
+
+    def download_data_parallel(self, years: list[int]):
+        # let's parallelize the download to save time
+        with ThreadPoolExecutor() as executor:  # in case needed, use the max_workers argument to limit the number of workers
+            futures = [executor.submit(self.download_one_year_data, year) for year in years]
+            for future in as_completed(futures):
+                year = future.result()
+                print(f"Completed download for year: {year}")
+
+def get_all_years() -> list[int]:
+    """
+    Get all years from 2000 until the present. 
+    """
+    now = datetime.datetime.now()
+    this_year = now.year
+    print(now, this_year)
+
+    all_years = list(range(2000, this_year+1))
+    print(all_years)
+    return all_years
+
+def main():
+
+    output_dir = "/shared/raw/soho"
+    base_url = "https://lasp.colorado.edu/eve/data_access/eve_data/lasp_soho_sem_data/long/15_sec_avg/"
+
+
+    years = get_all_years()
+
+
+    downloader = SohoDownloader(output_dir, base_url)
+    downloader.download_data_parallel(years)
+
+
+
+if __name__ == "__main__":
+    main()
