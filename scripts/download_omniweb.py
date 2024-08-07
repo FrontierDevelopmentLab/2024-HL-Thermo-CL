a2a5eef1131ff21555be0e395727080ca02718cd
Will Fawcett <willfaw@gmail.com>
2024-08-07 18:56:40 +0000

    modify to download on a per-month basis

--- scripts/download_omniweb.py
+++ scripts/download_omniweb.py
@@ -1,6 +1,8 @@
 import os
 import subprocess
 from datetime import datetime, timedelta
+import calendar
+
 
 def get_all_years() -> list[int]:
     """
@@ -27,9 +29,22 @@ def get_last_day_of_previous_month():
     return last_day_prev_month
 
 
+def get_start_and_end_dates_per_month(year: int, month: int):
+    """
+    Get the start date and end date for a given month and year. 
+    """
+
+    start_date = f"{year}0101"
+
+    # Get the last day of the month
+    last_day_of_month = calendar.monthrange(year, month)[1]
+
+    end_date = f"{year}{month:02}{last_day_of_month:02}"
+
+    return start_date, end_date
 
 
-def get_year_start_and_end_dates(year) -> tuple[str]:
+def get_year_start_and_end_dates_single_year(year) -> tuple[str]:
     """
     Correct end date should be either 31st of December (if the year isn't this year), 
     or the last date of the previous month.
@@ -56,23 +71,34 @@ def get_year_start_and_end_dates(year) -> tuple[str]:
 
     return start_date, end_date, year
 
+def download_omniweb_data_one_month(year:int, month:int, omiweb_data_dir: str): 
+    """
+    note year in YYYY format
+    """
+    start_date, end_date = get_start_and_end_dates_per_month(year, month)
+    # Generate the wget command
+    output_file_name = f"data_{year}_{month:02}.txt"
+    output_file = os.path.join(omiweb_data_dir, output_file_name)
+    return wget_omniweb_data(output_file, start_date, end_date)
+
+
 def download_omniweb_data_one_year(year: int, omiweb_data_dir: str):
+    """
+    note year in YYYY format
+    """
+    start_date, end_date, year = get_year_start_and_end_dates_single_year(year)
 
+    # Generate the wget command
+    output_file_name = f"data_{year}.txt"
+    output_file = os.path.join(omiweb_data_dir, output_file_name)
+    return wget_omniweb_data(output_file, start_date, end_date)
 
-    # constants
+def wget_omniweb_data(output_file, start_date, end_date):
     base_url = "https://omniweb.gsfc.nasa.gov/cgi/nx1.cgi"
     fixed_params = "activity=retrieve&res=min&spacecraft=omni_min&vars=11&vars=12&vars=13&vars=14&vars=15&vars=16&vars=17&vars=18&vars=19&vars=20&vars=21&vars=22&vars=23&vars=24&vars=25&vars=26&vars=27&vars=28&vars=29&vars=30&vars=31&vars=32&vars=33&vars=34&vars=35&vars=36&vars=37&vars=38&vars=39&vars=40&vars=41&vars=42&scale=Linear&ymin=&ymax=&view=0&charsize=&xstyle=0&ystyle=0&symbol=0&symsize=&linestyle=solid&table=0&imagex=640&imagey=480&color=&back="
 
     
-    start_date, end_date, year = get_year_start_and_end_dates(year)
-
-    # Generate the wget command
-    output_file_name = f"data_{year}.txt"
-    print(output_file_name)
-    output_file = os.path.join(omiweb_data_dir, output_file_name)
-    print('check if already exists')
     file_already_exists = os.path.exists(output_file)
-
     if not file_already_exists:
         wget_command = f"wget --post-data \"{fixed_params}&start_date={start_date}&end_date={end_date}\" {base_url} -O {output_file}"
     
