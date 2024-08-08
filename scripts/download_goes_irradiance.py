eba1430a23fb54d12a666891db7d39eb358f1164
Will Fawcett <willfaw@gmail.com>
2024-08-08 18:23:36 +0000

    functionalize

--- scripts/download_goes_irradiance.py
+++ scripts/download_goes_irradiance.py
@@ -25,22 +25,9 @@ def download_active_missions(output_dir, url2):
     this_year = datetime.now().year
     y16 = list(range(2017,this_year+1))
     y18 = list(range(2022,this_year+1))
-    
-    #GOES 16
-    for year in y16:
-        dataurl = url2 + 'goes16' + '/l2/data/euvs-l2-avg1m_science/sci_euvs-l2-avg1m_g16_y' + str(year) + '_v1-0-4.nc'
-        download_dir = output_dir + 'goes/goes16_y' + str(year) + '.nc'
-        wget_out_set = wget.download(dataurl,download_dir)
-        print(f'downloaded at {wget_out_set}')
 
-
-    #GOES 18
-    for year in y18:
-        dataurl = url2 + 'goes18' + '/l2/data/euvs-l2-avg1m_science/sci_euvs-l2-avg1m_g18_y' + str(year) + '_v1-0-4.nc'
-        download_dir = output_dir + 'goes/goes18_y' + str(year) + '.nc'
-        wget_out_set = wget.download(dataurl,download_dir)
-        print(f'downloaded at {wget_out_set}')
-
+    download_goes16(y16, output_dir, url2)
+    download_goes18(y18, output_dir, url2)
     
 
 def download_finished_missions(output_dir, url1, url2):
@@ -50,24 +37,51 @@ def download_finished_missions(output_dir, url1, url2):
     y15 = list(range(2010,2020+1))
     y17 = list(range(2018,2023+1))
 
-    #GOES 14
+    download_goes14(y14=y14, output_dir=output_dir, url1=url1)
+    download_goes15(y15=y15, output_dir=output_dir, url1=url1)
+    download_goes17(y17=y17, output_dir=output_dir, url2=url2)
+
+
+def download_goes14(y14, output_dir, url1):
+    #GOES 14 (not operational in 2024)
     for year in y14:
         dataurl = url1 + 'goes14' + '/geuv-l2-avg1m/sci_geuv-l2-avg1m_g14_y' + str(year) + '_v5-0-0.nc'
         download_dir = output_dir + 'goes/goes14_y' + str(year) + '.nc'
         wget_out_set = wget.download(dataurl,download_dir)
         print(f'downloaded at {wget_out_set}')
 
-    #GOES 15
+def download_goes15(y15, output_dir, url1):
+    #GOES 15 (not operational in 2024)
     for year in y15:
         dataurl = url1 + 'goes15' + '/geuv-l2-avg1m/sci_geuv-l2-avg1m_g15_y' + str(year) + '_v5-0-0.nc'
         download_dir = output_dir + 'goes/goes15_y' + str(year) + '.nc'
         wget_out_set = wget.download(dataurl,download_dir)
         print(f'downloaded at {wget_out_set}')
 
-    #GOES 17
+def download_goes16(y16, output_dir, url2):
+    #GOES 16 (operational in 2024)
+    for year in y16:
+        dataurl = url2 + 'goes16' + '/l2/data/euvs-l2-avg1m_science/sci_euvs-l2-avg1m_g16_y' + str(year) + '_v1-0-4.nc'
+        download_dir = output_dir + 'goes/goes16_y' + str(year) + '.nc'
+        wget_out_set = wget.download(dataurl,download_dir)
+        print(f'downloaded at {wget_out_set}')
+
+
+def download_goes17(y17, output_dir, url2):
+    #GOES 17 (not operational in 2024)
     for year in y17:
         dataurl = url2 + 'goes17' + '/l2/data/euvs-l2-avg1m_science/sci_euvs-l2-avg1m_g17_y' + str(year) + '_v1-0-4.nc'
         download_dir = output_dir + 'goes/goes17_y' + str(year) + '.nc'
+        wget_out_set = wget.download(dataurl,download_dir)
+        print(f'downloaded at {wget_out_set}')
+
+
+
+def download_goes18(y18, output_dir, url2):
+    #GOES 18 (operational in 2024)
+    for year in y18:
+        dataurl = url2 + 'goes18' + '/l2/data/euvs-l2-avg1m_science/sci_euvs-l2-avg1m_g18_y' + str(year) + '_v1-0-4.nc'
+        download_dir = output_dir + 'goes/goes18_y' + str(year) + '.nc'
         wget_out_set = wget.download(dataurl,download_dir)
         print(f'downloaded at {wget_out_set}')
 
