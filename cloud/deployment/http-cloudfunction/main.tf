56934b2d4036d72e80b72207ff46be25b8870fa7
Will Fawcett <willfaw@gmail.com>
2024-08-10 19:16:17 +0000

    WIP: http cloud function

---
+++ cloud/deployment/http-cloudfunction/main.tf
@@ -0,0 +1,52 @@
+data "archive_file" "function_archive_file" {
+  type        = "zip"
+  output_path = "/tmp/${var.zip_file_name}"
+  source_dir  = var.code_source_dir
+}
+
+# Object inside bucket, corresponding to zipped file for cloud function
+resource "google_storage_bucket_object" "function_zip_object" {
+  name   = var.zip_file_name #"function-source-ingestion.zip"
+  bucket = var.function_bucket_name
+  source = data.archive_file.function_archive_file.output_path # Path to the zipped function source code
+}
+
+resource "google_cloudfunctions2_function" "http_function" {
+  name        = var.function_name
+  location    = var.region
+  description = "A simple HTTP-triggered Cloud Function"
+  labels      = var.labels
+  project     = var.project_id
+
+  build_config {
+    entry_point = var.function_entrypoint_name
+    runtime     = "python311"
+
+    # Build-time environment variables
+    environment_variables = {
+      BUILD_CONFIG_TEST = "build_test"
+      IS_CLOUD          = "true"
+    }
+
+    # Location of the function source code 
+    source {
+      storage_source {
+        bucket = var.function_bucket_name
+        object = google_storage_bucket_object.function_zip_object.name
+      }
+    }
+  }
+
+  service_config {
+    service_account_email = var.service_account_email
+    available_memory = "512Mi"
+    timeout_seconds  = 60
+    environment_variables = {
+      IS_CLOUD = "true"
+    }
+  }
+
+  # event_trigger {
+  #   event_type = "http"
+  # }
+}
---
+++ cloud/deployment/http-cloudfunction/variables.tf
@@ -0,0 +1,37 @@
+
+variable "function_name" {
+  description = "Name of the cloud function"
+}
+
+variable "zip_file_name" {
+  description = "Name of the zipped file to store the cloud function source code"
+}
+
+variable "function_bucket_name" {
+  description = "Name of the bucket to store the cloud function source code"
+}
+
+variable "code_source_dir" {
+  description = "Path to the directory containing the cloud function source code (on the local machine)"
+}
+
+variable "labels" {
+  description = "Common labels to be passed to resources"
+  type        = map(string)
+}
+
+variable "function_entrypoint_name" {
+  description = "Name of the function entrypoint"
+}
+
+variable "service_account_email" {
+  description = "Email of the service account to use for the cloud function"
+}
+
+variable "region" {
+  description = "Region to deploy resources to"
+}
+
+variable "project_id" {
+  description = "Project ID"
+}
