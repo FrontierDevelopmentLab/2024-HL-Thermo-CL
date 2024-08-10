9a87ef3cb456ba46c612f48e5138aa6dfa3f99d8
Will Fawcett <willfaw@gmail.com>
2024-08-10 19:18:21 +0000

    setup code for messages

--- cloud/deployment/src/satellite_data/message/main.py
+++ cloud/deployment/src/satellite_data/message/main.py
@@ -1,14 +1,12 @@
 """
-Send messages to the ingest-raw-satellite-data topic in the hl-therm project.
-This will begin the data download process for the specified satellite, taken from the tudelft ftp server.
-
-Files on the ftp server are only updated once per month.
+This cloud function is used to trigger the ingestion chain for the "satellite_data".
+The function is triggered by an HTTP request, and sends a message to the "ingest-raw-satellite-data" topic.
 """
 
+import functions_framework
 from karman.io import CloudMessageHandler
 from karman.enums import SatelliteCollection
 from karman.enums import Satellite
-
 
 
 def create_a_message(satellite: Satellite) -> dict:
@@ -22,17 +20,16 @@ def create_a_message(satellite: Satellite) -> dict:
     return message
 
 
-def main():
+@functions_framework.http
+def hello_http(request):
+    
+    print("Received HTTP request to trigger download of satellite data.")
 
-    # Send a message to download the raw data for each satellite
     project_id = 'hl-therm'
     topic_name = 'tf-ingest-raw-satellite-data'
     messenger = CloudMessageHandler(project_id, topic_name)
 
+    # Send message to download all satellite data. 
     for satellite in SatelliteCollection.get_all_satellites():
         raw_message = create_a_message(satellite)
         messenger.send_message(raw_message, debug=False)    
-
-
-if __name__ == "__main__":
-    main()
